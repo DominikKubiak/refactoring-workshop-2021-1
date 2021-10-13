209dc53a0060ab324c2f61bcd325651a3b26fffb
Dominik Kubiak <DominikKubiak@outlook.com>
2021-10-13 18:11:55 +0200

    Added function createNewHead()

--- SnakeController/SnakeController.hpp
+++ SnakeController/SnakeController.hpp
@@ -12,27 +12,16 @@ class IPort;
 
 namespace Snake
 {
-struct ConfigurationError : std::logic_error
-{
-    ConfigurationError();
-};
+    struct ConfigurationError : std::logic_error
+    {
+        ConfigurationError();
+    };
 
-struct UnexpectedEventException : std::runtime_error
-{
-    UnexpectedEventException();
-};
+    struct UnexpectedEventException : std::runtime_error
+    {
+        UnexpectedEventException();
+    };
 
-class Controller : public IEventHandler
-{
-public:
-    Controller(IPort& p_displayPort, IPort& p_foodPort, IPort& p_scorePort, std::string const& p_config);
-
-    Controller(Controller const& p_rhs) = delete;
-    Controller& operator=(Controller const& p_rhs) = delete;
-
-    void receive(std::unique_ptr<Event> e) override;
-
-private:
     struct Segment
     {
         int x;
@@ -40,15 +29,29 @@ private:
         int ttl;
     };
 
-    IPort& m_displayPort;
-    IPort& m_foodPort;
-    IPort& m_scorePort;
+    class Controller : public IEventHandler
+    {
+    public:
+        Controller(IPort &p_displayPort, IPort &p_foodPort, IPort &p_scorePort, std::string const &p_config);
 
-    std::pair<int, int> m_mapDimension;
-    std::pair<int, int> m_foodPosition;
+        Controller(Controller const &p_rhs) = delete;
+        Controller &operator=(Controller const &p_rhs) = delete;
 
-    Direction m_currentDirection;
-    std::list<Segment> m_segments;
-};
+        void receive(std::unique_ptr<Event> e) override;
+
+    protected:
+        Segment createNewHead();
+
+    private:
+        IPort &m_displayPort;
+        IPort &m_foodPort;
+        IPort &m_scorePort;
+
+        std::pair<int, int> m_mapDimension;
+        std::pair<int, int> m_foodPosition;
+
+        Direction m_currentDirection;
+        std::list<Segment> m_segments;
+    };
 
 } // namespace Snake
